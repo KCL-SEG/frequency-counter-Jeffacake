1c40fe000b8c5c55430b309affcb01a0e388f877
Jeff Jones <jeffbjones777@gmail.com>
2023-10-01 00:23:08 +0100

    Frequencies solution

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,23 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        item = str(item)
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
+
     return frequencies
+
+test1 = frequencies(['a', 'a', 'b', 'b', 'b', 'c'])
+print(test1)
+
+test2 = frequencies([100, 'Hello', '100', '100', 100])
+print(test2)
+
+
+
+
+
+
